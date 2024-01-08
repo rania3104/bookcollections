6add140396b1d9600946785937ed8b195782f3a3
rania3104 <raniaazizfarooqi.14574.11g@gmail.com>
2024-01-08 12:04:12 +0400

    progress ig

--- scripts.js
+++ scripts.js
@@ -6,7 +6,7 @@
                 return (
                     rect.top >= 0 &&
                     rect.left >= 0 &&
-                    rect.bottom <= (900 || 0) &&
+                    rect.bottom <= (1100 || 0) &&
                     rect.right <= (window.innerWidth || document.documentElement.clientWidth)
                 );
             }
